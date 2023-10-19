3bcaa835747e0450c751c5a7fe93fc8cc2d44db1
Eric Pineda <ericvpineda@berkeley.edu>
2023-10-19 14:34:26 -0700

    updated -- index.js comments and styling

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-function promptSplitter({ prompt, splitLength, newLine = false }) {
+/* Returns array of string chunks. 
+   Parameters:
+   - prompt: non-empty string 
+   - splitLength: positive integer value that dictates character length of string to split
+   - newLine: boolean that appends newline characters. Helpful when printing prompt chunks. 
+*/
+function promptSplitter({ prompt, splitLength, newLine=false }) {
+
+  // Parameter validation
   if (typeof splitLength != "number") {
     throw new TypeError("splitLength parameter must be of type number.");
   } else if (typeof prompt != "string") {
@@ -6,29 +14,29 @@ function promptSplitter({ prompt, splitLength, newLine = false }) {
   } else if (typeof newLine != "boolean") {
     throw new TypeError("newLine parameter must be of type boolean.");
   } else if (prompt.length === 0) {
-    throw new Error("prompt cannot be empty.");
+    throw new Error("prompt paramter cannot be empty.");
   } else if (splitLength <= 0) {
-    throw new Error("Split length must be greater than 0 characters.");
+    throw new Error("splitLength parameter must be greater than 0 characters.");
   }
 
   prompt = prompt.trim();
+  const SIZE = Math.ceil(prompt.length / splitLength);
   const newLineChar = newLine ? "\n" : " ";
+  const intro = `The total length of text I want to send you is too large to send in a single message.${newLineChar}I will now send you the text, and will follow this rule:${newLineChar}${newLineChar}[START PART 1/${SIZE}]${newLineChar}This content will be part 1 out of ${SIZE}.${newLineChar}[END PART 1/${SIZE}]${newLineChar}${newLineChar}Then you will answer: "Received part 1/${SIZE}"${newLineChar}Note: When I tell you "ALL PARTS SENT", then you can process all messages and answer my requests.${newLineChar}`;
 
-  const size = Math.ceil(prompt.length / splitLength);
+  // Return array of strings 
   const result = [];
-
-  const intro = `The total length of text I want to send you is too large to send in a single message.${newLineChar}I will now send you the text, and will follow this rule:${newLineChar}${newLineChar}[START PART 1/${size}]${newLineChar}This content will be part 1 out of ${size}.${newLineChar}[END PART 1/${size}]${newLineChar}${newLineChar}Then you will answer: "Received part 1/${size}"${newLineChar}Note: When I tell you "ALL PARTS SENT", then you can process all messages and answer my requests.${newLineChar}`;
-
   result.push(intro);
 
-  for (let i = 1, ct = 0; i <= size; i++, ct += splitLength) {
+  // Chunk input text into SIZE length
+  for (let i = 1, ct = 0; i <= SIZE; i++, ct += splitLength) {
     const message = prompt.slice(ct, ct + splitLength).trim();
-
-    let body = `[START PART ${i}/${size}]${newLineChar}${message}${newLineChar}[END PART ${i}/${size}]${newLineChar}`;
-    if (i == size) {
+    let body = `[START PART ${i}/${SIZE}]${newLineChar}${message}${newLineChar}[END PART ${i}/${SIZE}]${newLineChar}`;
+    
+    if (i == SIZE) {
       body += `ALL PARTS SENT. You may now continue processing requests`;
     } else {
-      body += `Reply with "Received part ${i}/${size}". Remember, do not reply anything else yet and wait for next part.${newLineChar}`;
+      body += `Reply with "Received part ${i}/${SIZE}". Remember, do not reply anything else yet and wait for next part.${newLineChar}`;
     }
 
     result.push(body.trim());
